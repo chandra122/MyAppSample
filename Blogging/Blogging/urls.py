27eb85b4bf74785c43bb17829cc25a29210dbde1
chandra122 <sekharchandu474@gmail.com>
2020-04-13 22:42:35 +0530

    Users Created

--- Blogging/Blogging/urls.py
+++ Blogging/Blogging/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from Blogging.users import views as test
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('register/', test.register, name='register'),
     path('blog/', include('Blog.urls')),
 ]
